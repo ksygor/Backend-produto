c85874fa60f595d6ab19043e0a6881f6b9b7ca56
ksygor <ygorfods@hotmail.com>
2021-04-09 20:49:45 -0300

    “Backend-produto”

---
+++ src/models/produto.js
@@ -0,0 +1,37 @@
+// Define que estamos utilizando o sequelize
+const Sequelize = require('sequelize');
+ 
+// Obtem dados de conexão entre sequelize e banco de dados MySQL
+const sequelize = require('../database/database.js');
+ 
+// Cria tabela no BD e seus campos
+const Produto = sequelize.define("produto", {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    nome: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+        validate: {
+            len: [3, 100]
+        }
+    },
+    descricao: {
+        allowNull: false,
+        type: Sequelize.STRING(255),          
+        
+    },
+    preco: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+    },
+    qtdemEstoque: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+        }
+    });
+
+    module.exports = Produto;
